a17174250e1a484be9632328b0d4bca334150e3f
AdhirajSingh-ops <adhiraj.singh3@student.tdsb.on.ca>
2026-02-18 02:03:46 +0000

    done questions

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -5,5 +5,59 @@ public class PracticeProblem {
 	}
 
 	//Write your functions here
-
+    public static String evenOrOdd(int number) {
+		if (number % 2 == 0) {
+			return "Even";
+		} else {
+			return "Odd";
+		}	
 }
+
+public static String teacherOrStudent(String name) {
+	if (name.equals("Kalisz")) {
+		return "Teacher";
+	} else {
+		return "Student";
+	}
+}
+
+ public static int fartherFromZero (int value) {
+	if (value > 0) {
+		return value + 5;
+	} else if (value < 0) {
+		return value - 5;
+	} else {
+		return 0;
+	}
+ }
+
+
+ public static String isFive(int number) {
+        if (number == 5) {
+            return "The number is Five";
+        } else {
+            return "The number is not Five";
+        }
+    }
+public static String positiveOrNegative(int value) {
+		if  (value > 0) {
+			return "Positive";
+		} else {
+			return "Negative";
+		}
+	}
+	public static String highOrLow(int value) {
+		if (value > 100) {
+			return "High";
+		} else {
+			return "Low";
+		}
+	}
+	public static String isHello(String word) {	
+		if (word.equals("Hello")) {
+			return "The word is Hello";
+		} else {
+			return "The word is not Hello";
+		}
+	}
+	}
